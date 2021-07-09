57d11b7e9f43b8f322324a732004c8fea7e66992
hacklover <info@hacklover.net>
2021-07-09 05:41:32 +0200

    Initial commit

---
+++ components/SystemBar/components/SystemBarMenuContent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="owd-desktop__system-bar__content">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+const props = defineProps({
+  config: Object
+})
+</script>
+
+<style scoped lang="scss">
+.owd-desktop__system-bar__content {
+  position: absolute;
+  background: $owd-desktop-system-bar-menu-content-background;
+  box-shadow: 0 0 0 1px $owd-desktop-system-bar-menu-content-border-color;
+  color: $owd-desktop-system-bar-menu-content-color;
+  border-radius: 5px;
+  z-index: 999;
+}
+</style>
+
+<style lang="scss">
+.owd-desktop {
+  &__system-bar {
+    &--position-top {
+      .owd-desktop__system-bar__content {
+        top: 48px;
+      }
+    }
+
+    &--position-bottom {
+      .owd-desktop__system-bar__content {
+        bottom: 48px;
+      }
+    }
+  }
+}
+</style>
---
+++ modules/NotificationMenu/components/NotificationContent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <DesktopSystemBarMenuContent
+        v-if="opened"
+        class="owd-desktop__notification__container"
+        v-click-outside="menuClose"
+    >
+
+      <NotificationList class="col col-notification-list" />
+
+    </DesktopSystemBarMenuContent>
+
+    <NotificationListFloating v-show="!opened" />
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmit } from 'vue'
+import DesktopSystemBarMenuContent from '../../../components/SystemBar/components/SystemBarMenuContent.vue'
+import NotificationList from "./NotificationList/NotificationList.vue";
+import NotificationListFloating from "./NotificationListFloating/NotificationListFloating.vue";
+
+const props = defineProps({
+  opened: Boolean
+})
+
+const emit = defineEmit([
+  'close'
+])
+
+function menuClose() {
+  emit('close')
+}
+</script>
+
+<style scoped lang="scss">
+.owd-desktop__notification__container {
+  display: grid;
+  width: 500px;
+  max-width: calc(100% - 32px);
+  height: 500px;
+  max-height: 60vh;
+  line-height: 32px;
+  padding: 16px 0;
+  left: 50%;
+  transform: translateX(-50%);
+
+  .col-notification-list {
+    padding: 8px 24px;
+
+    @media(max-width: 600px), (max-height: 400px) {
+      padding: 4px 16px;
+    }
+  }
+}
+</style>
